0dfbaa56e534b8c11a05f05a7582733e5a96e3be
user.name "Aritra Bag <user.email "aritrabag5427@gmail.com>
2026-05-31 18:55:26 +0530

    Initial Commit

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Banner from '../components/home/Banner'
+import Hero from '../components/home/Hero'
+import Feature from '../components/home/Feature'
+import CallToAction from '../components/home/CallToAction'
+import Footer from '../components/home/Footer'
+
+const Home = () => {
+  return (
+    <div>
+      <Banner/>
+      <Hero/>
+      <Feature/>
+      <CallToAction/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default Home
